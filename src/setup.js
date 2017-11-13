74e4879b2f0aa0bd9c807222922f6f6d72712b4b
Juan Cruz Viotti <jv@jviotti.com>
2017-11-13 18:46:59 -0400

    Get rid of mocha.opts and setup.js
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/setup.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const path = require('path')
-
-global.options = require(path.join(__dirname, '../user.json'))
-global.assetDir = path.resolve(__dirname, '../assets')
-
-global.provDevice = null
