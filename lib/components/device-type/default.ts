26e106a165fbf436b8f533ccebbbaf77475b2e3b
Juan Cruz Viotti <jv@jviotti.com>
2017-11-27 14:26:08 -0400

    Don't wait for stdin on provision before continuing
    We can't access stdin from Ava, so we'll rely on polling the API for
    now.
    
    Change-Type: patch
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- lib/components/device-type/default.ts
+++ lib/components/device-type/default.ts
@@ -1,12 +1,6 @@
 'use strict'
 
-import Bluebird = require('bluebird')
-
 exports.provision = async (writer, imagePath, options) => {
   await writer.writeImage(imagePath, options.destination)
   console.log(`Write complete. Please remove ${options.destination}, plug it into the device, and turn it on.`)
-  return new Bluebird((resolve) => {
-    console.log('Press any key to continue.')
-    process.stdin.once('data', resolve)
-  })
 }
