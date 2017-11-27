565f05c70691b1961209ddf78089bfdb1e341e5f
Juan Cruz Viotti <jv@jviotti.com>
2017-11-27 13:36:17 -0400

    Get all options from environment variables
    Change-Type: patch
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- lib/main.ts
+++ lib/main.ts
@@ -8,36 +8,44 @@ import os = require('os')
 import path = require('path')
 
 const utils = require('./utils')
-const options = require(path.join(__dirname, '../user.json'))
 const resinio = utils.requireComponent('resinio', 'sdk')
 const resinos = utils.requireComponent('resinos', 'default')
 const writer = utils.requireComponent('writer', 'etcher')
+
+const options = <any>{
+  deviceType: process.env.DEVICE_TYPE,
+  resinOSVersion: process.env.RESINOS_VERSION,
+  applicationName: process.env.APPLICATION_NAME,
+  disk: process.env.DISK,
+  email: process.env.EMAIL,
+  password: process.env.PASSWORD
+}
+
 const deviceType = utils.requireComponent('device-type', options.deviceType)
 
 ava.test.before(async () => {
   const imagePath = path.join(os.tmpdir(), 'resin.img')
-  const applicationName = process.env.APPLICATION_NAME
   const configuration = {
     network: 'ethernet'
   }
 
   console.log('Logging into resin.io')
   await resinio.loginWithCredentials({
-    email: process.env.EMAIL,
-    password: process.env.PASSWORD
+    email: options.email,
+    password: options.password
   })
 
-  console.log(`Removing application: ${applicationName}`)
-  await resinio.removeApplication(applicationName)
+  console.log(`Removing application: ${options.applicationName}`)
+  await resinio.removeApplication(options.applicationName)
 
-  console.log(`Creating application: ${applicationName} with device type ${options.deviceType}`)
-  await resinio.createApplication(applicationName, options.deviceType)
+  console.log(`Creating application: ${options.applicationName} with device type ${options.deviceType}`)
+  await resinio.createApplication(options.applicationName, options.deviceType)
 
   console.log('Downloading device type OS')
   await resinio.downloadDeviceTypeOS(options.deviceType, options.version, imagePath)
 
   console.log('Getting application OS configuration')
-  const applicationConfiguration = await resinio.getApplicationOSConfiguration(applicationName, configuration)
+  const applicationConfiguration = await resinio.getApplicationOSConfiguration(options.applicationName, configuration)
 
   console.log(`Injecting resin.io configuration into ${imagePath}`)
   await resinos.injectResinConfiguration(imagePath, applicationConfiguration)
@@ -57,7 +65,7 @@ ava.test.before(async () => {
 let provDevice = null
 
 ava.test.skip('device should become online', async (test) => {
-  const devices = await resinio.getApplicationDevices(process.env.APPLICATION_NAME)
+  const devices = await resinio.getApplicationDevices(options.applicationName)
   test.is(devices.length, 1)
   test.true(devices instanceof Array)
   provDevice = devices[0].id
@@ -73,8 +81,8 @@ ava.test.skip('device should report hostOS version', async (test) => {
 ava.test.skip('should push an application', async (test) => {
   const repositoryPath = path.join(os.tmpdir(), 'test')
   await fse.remove(repositoryPath)
-  const repository = await git.Clone(options.gitAppURL, repositoryPath)
-  const remote = await resinio.getApplicationGitRemote(process.env.APPLICATION_NAME)
+  const repository = await git.Clone('https://github.com/alexandrosm/hello-python', repositoryPath)
+  const remote = await resinio.getApplicationGitRemote(options.applicationName)
   await git.Remote.create(repository, 'resin', remote)
   await remote.push([ 'refs/heads/master:refs/heads/master' ], {
     callbacks: {
