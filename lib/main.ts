4fe7c62a3bc6e4d097214b8a88c8f436e4d25e09
Juan Cruz Viotti <jv@jviotti.com>
2017-11-27 11:29:29 -0400

    Start using TypeScript async/await
    Change-Type: patch
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- lib/main.ts
+++ lib/main.ts
@@ -14,88 +14,61 @@ const resinos = utils.requireComponent('resinos', 'default')
 const writer = utils.requireComponent('writer', 'etcher')
 const deviceType = utils.requireComponent('device-type', options.deviceType)
 
-let provDevice = null
-
-ava.test.before(() => {
+ava.test.before(async () => {
   const imagePath = path.join(os.tmpdir(), 'resin.img')
   const applicationName = process.env.APPLICATION_NAME
   const configuration = {
     network: 'ethernet'
   }
 
-  return resinio.loginWithCredentials({
+  await resinio.loginWithCredentials({
     email: process.env.EMAIL,
     password: process.env.PASSWORD
-  }).then(() => {
-    return resinio.hasApplication(applicationName)
-  }).then((hasApplication) => {
-    if (hasApplication) {
-      return resinio.removeApplication(applicationName)
-    }
-
-    return Bluebird.resolve()
-  }).then(() => {
-    return resinio.createApplication(applicationName, options.deviceType)
-  }).then(() => {
-    return resinio.downloadDeviceTypeOS(options.deviceType, options.version, imagePath)
-  }).then(() => {
-    return resinio.getApplicationOSConfiguration(applicationName, configuration).then((applicationConfiguration) => {
-      return resinos.injectResinConfiguration(imagePath, applicationConfiguration)
-    })
-  }).then(() => {
-    return resinos.injectNetworkConfiguration(imagePath, configuration)
-  }).then(() => {
-    return deviceType.provision(writer, imagePath, {
-      destination: options.disk
-    })
   })
-})
 
-ava.test.skip('device should become online', (test) => {
-  return Bluebird.delay(10000)
-    .return(process.env.APPLICATION_NAME)
-    .then(resinio.getApplicationDevices)
-    .then((devices) => {
-      test.is(devices.length, 1)
-      test.true(devices instanceof Array)
-      return devices[0]
-    })
-    .then((device) => {
-      provDevice = device.id
-      return resinio.isDeviceOnline(device)
-    }).then((isOnline) => {
-      return test.true(isOnline)
-    })
-})
-
-ava.test.skip('device should report hostOS version', (test) => {
-  return resinio.getDeviceHostOSVersion(provDevice).then((version) => {
-    test.is(version, 'Resin OS 2.0.6+rev3')
+  await resinio.removeApplication(applicationName)
+  await resinio.createApplication(applicationName, options.deviceType)
+  await resinio.downloadDeviceTypeOS(options.deviceType, options.version, imagePath)
+  const applicationConfiguration = await resinio.getApplicationOSConfiguration(applicationName, configuration)
+  await resinos.injectResinConfiguration(imagePath, applicationConfiguration)
+  await resinos.injectNetworkConfiguration(imagePath, configuration)
+  await deviceType.provision(writer, imagePath, {
+    destination: options.disk
   })
+
+  await Bluebird.delay(10000)
 })
 
-ava.test.skip('should push an application', (test) => {
-  let repository = null
+let provDevice = null
 
-  const repoPath = path.join(os.tmpdir(), 'test')
-  return fse.remove(repoPath).then(() => {
-    return git.Clone(options.gitAppURL, repoPath)
-  }).then((repo) => {
-    repository = repo
-    return resinio.getApplicationGitRemote(process.env.APPLICATION_NAME)
-  }).then((remote) => {
-    return git.Remote.create(repository, 'resin', remote)
-  }).then((remote) => {
-    return remote.push([ 'refs/heads/master:refs/heads/master' ], {
-      callbacks: {
-        credentials: (_url, user) => {
-          return git.Cred.sshKeyFromAgent(user)
-        }
+ava.test.skip('device should become online', async (test) => {
+  const devices = await resinio.getApplicationDevices(process.env.APPLICATION_NAME)
+  test.is(devices.length, 1)
+  test.true(devices instanceof Array)
+  provDevice = devices[0].id
+  const isOnline = await resinio.isDeviceOnline(devices[0])
+  test.true(isOnline)
+})
+
+ava.test.skip('device should report hostOS version', async (test) => {
+  const version = await resinio.getDeviceHostOSVersion(provDevice)
+  test.is(version, 'Resin OS 2.0.6+rev3')
+})
+
+ava.test.skip('should push an application', async (test) => {
+  const repositoryPath = path.join(os.tmpdir(), 'test')
+  await fse.remove(repositoryPath)
+  const repository = await git.Clone(options.gitAppURL, repositoryPath)
+  const remote = await resinio.getApplicationGitRemote(process.env.APPLICATION_NAME)
+  await git.Remote.create(repository, 'resin', remote)
+  await remote.push([ 'refs/heads/master:refs/heads/master' ], {
+    callbacks: {
+      credentials: (_url, user) => {
+        return git.Cred.sshKeyFromAgent(user)
       }
-    })
-  }).then(() => {
-    return resinio.getDeviceCommit(provDevice).then((commit) => {
-      return test.is(commit.length, 40)
-    })
+    }
   })
+
+  const commit = await resinio.getDeviceCommit(provDevice)
+  test.is(commit.length, 40)
 })
