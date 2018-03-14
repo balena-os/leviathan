7f704a74635034e6ef19764319ccdd0e7985fb78
Theodor Gherzan <theodor@resin.io>
2018-03-14 10:06:57 +0000

    Produce, consume and format TAP output
    Change-type: patch
    Signed-off-by: Theodor Gherzan <theodor@resin.io>

--- entry.sh
+++ entry.sh
@@ -2,6 +2,8 @@
 
 trap 'echo "Teardown..." ; node ./scripts/teardown.js' EXIT 
 
+export RESINOS_TESTS_RESULTS_PATH="$RESINOS_TESTS_TMPDIR/result.tap"
+
 RESINOS_TESTS_APPLICATION_NAME=$(
   set -m
   node ./scripts/resolve.js APPLICATION_NAME "${RESINOS_TESTS_APPLICATION_NAME}" & wait $!
@@ -16,4 +18,5 @@ RESINOS_TESTS_RESINOS_VERSION_UPDATE=$(
 )
 
 echo "Running tests..."
-npm start
+npm  start --silent | tee $RESINOS_TESTS_RESULTS_PATH
+RESINOS_TESTS_RESULTS_PATH=$(node ./scripts/format.js mdFormatResults $RESINOS_TESTS_RESULTS_PATH)
