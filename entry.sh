84380a28ca2d26bf49dd7f90183e7e79baaadff4
Theodor Gherzan <theodor@resin.io>
2018-04-13 10:18:22 +0000

    Remove trailing characters
    Signed-off-by: Theodor Gherzan <theodor@resin.io>

--- entry.sh
+++ entry.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-trap 'echo "Teardown..." ; node ./scripts/teardown.js' EXIT 
+trap 'echo "Teardown..." ; node ./scripts/teardown.js' EXIT
 
 export RESINOS_TESTS_APPLICATION_NAME=$(
   set -m
   node ./scripts/resolve.js APPLICATION_NAME "${RESINOS_TESTS_APPLICATION_NAME}" & wait $!
 )
 export RESINOS_TESTS_RESINOS_VERSION=$(
-  set -m 
+  set -m
   node ./scripts/resolve.js RESINOS_VERSION "${RESINOS_TESTS_RESINOS_VERSION}" "${RESINOS_TESTS_DEVICE_TYPE}" & wait $!
 )
 export RESINOS_TESTS_RESINOS_VERSION_UPDATE=$(
-  set -m 
+  set -m
   node ./scripts/resolve.js RESINOS_VERSION "${RESINOS_TESTS_RESINOS_VERSION_UPDATE-*}" "${RESINOS_TESTS_DEVICE_TYPE}" & wait $!
 )
 export RESINOS_TESTS_SSH_KEY_LABEL=${RESINOS_TESTS_APPLICATION_NAME}
