ee060ddc1807ecefeaebc39cfeaf0bc7b0ed1663
Theodor Gherzan <theodor@resin.io>
2019-04-16 16:50:16 +0100

    Refactor Suite to allow multiple setup steps
    Signed-off-by: Theodor Gherzan <theodor@resin.io>

--- suites/testlodge/testlodge.suite.js
+++ suites/testlodge/testlodge.suite.js
@@ -6,41 +6,42 @@
 
 'use strict';
 
-module.exports = {
-  setup: async (root, options) => {
-    const noop = require('lodash/noop');
+module.exports = [
+  {
+    setup: async function() {
+      const noop = require('lodash/noop');
 
-    const Bluebird = require('bluebird');
-    const fse = require('fs-extra');
-    const { join } = require('path');
-    const { homedir } = require('os');
+      const Bluebird = require('bluebird');
+      const fse = require('fs-extra');
+      const { join } = require('path');
+      const { homedir } = require('os');
 
-    const utils = require(join(root, 'lib', 'common', 'utils'));
-    const Teardown = require(join(root, 'lib', 'common', 'teardown'));
-    const Worker = require(join(root, 'lib', 'workers', options.worker.type));
-    const BalenaOS = require(join(root, 'lib', 'components', 'os', 'balenaos'));
-    const Balena = require(join(root, 'lib', 'components', 'balena', 'sdk'));
+      const Worker = require(join(this.frameworkPath, 'workers', this.options.worker.type));
+      const BalenaOS = require(join(this.frameworkPath, 'components', 'os', 'balenaos'));
+      const Balena = require(join(this.frameworkPath, 'components', 'balena', 'sdk'));
 
-    const teardown = new Teardown();
+      this.context = { utils: require(join(this.frameworkPath, 'common', 'utils')) };
 
-    return Bluebird.try(async () => {
-      fse.ensureDirSync(options.tmpdir);
-      const deviceType = require(join(
-        root,
-        'contracts',
-        'contracts',
-        'hw.device-type',
-        options.deviceType,
-        'contract.json'
-      ));
+      fse.ensureDirSync(this.options.tmpdir);
+      this.context = {
+        deviceType: require(join(
+          this.frameworkPath,
+          '..',
+          'contracts',
+          'contracts',
+          'hw.device-type',
+          this.options.deviceType,
+          'contract.json'
+        ))
+      };
 
-      const sshKeyPath = join(homedir(), 'id');
+      this.context = { sshKeyPath: join(homedir(), 'id') };
 
-      const sdk = new Balena(options.balena.apiUrl);
+      this.context = { balena: { sdk: new Balena(this.options.balena.apiUrl) } };
 
-      await sdk.loginWithToken(options.balena.apiKey);
-      teardown.register(() => {
-        return sdk.logout().catch(
+      await this.context.balena.sdk.loginWithToken(this.options.balena.apiKey);
+      this.teardown.register(() => {
+        return this.context.balena.sdk.logout().catch(
           {
             code: 'BalenaNotLoggedIn'
           },
@@ -48,12 +49,18 @@ module.exports = {
         );
       });
 
-      await sdk.createApplication(options.balena.application.name, deviceType.slug, {
-        delta: options.balena.application.env.delta
-      });
-      teardown.register(() => {
-        return sdk
-          .removeApplication(options.balena.application.name)
+      this.context = { balena: { application: this.options.balena.application } };
+
+      await this.context.balena.sdk.createApplication(
+        this.context.balena.application.name,
+        this.context.deviceType.slug,
+        {
+          delta: this.context.balena.application.env.delta
+        }
+      );
+      this.teardown.register(() => {
+        return this.context.balena.sdk
+          .removeApplication(this.context.balena.application.name)
           .catch(
             {
               code: 'BalenaNotLoggedIn'
@@ -68,9 +75,14 @@ module.exports = {
           );
       });
 
-      await sdk.addSSHKey(options.balena.sshKeyLabel, await utils.createSSHKey(sshKeyPath));
-      teardown.register(() => {
-        return Bluebird.resolve(sdk.removeSSHKey(options.balena.sshKeyLabel)).catch(
+      await this.context.balena.sdk.addSSHKey(
+        this.options.balena.sshKeyLabel,
+        await this.context.utils.createSSHKey(this.context.sshKeyPath)
+      );
+      this.teardown.register(() => {
+        return Bluebird.resolve(
+          this.context.balena.sdk.removeSSHKey(this.options.balena.sshKeyLabel)
+        ).catch(
           {
             code: 'BalenaNotLoggedIn'
           },
@@ -78,94 +90,88 @@ module.exports = {
         );
       });
 
-      const uuid = await sdk.generateUUID();
-      const deviceApiKey = await sdk.register(options.balena.application.name, uuid);
+      this.context = { balena: { uuid: await this.context.balena.sdk.generateUUID() } };
+      this.context = {
+        deviceApiKey: await this.context.balena.sdk.register(
+          this.context.balena.application.name,
+          this.context.balena.uuid
+        )
+      };
 
-      const os = new BalenaOS({
-        deviceType: deviceType.slug,
-        download: {
-          type: options.balenaOS.download.type,
-          version: options.balenaOS.download.version,
-          source: options.balenaOS.download.source
-        },
-        network: options.balenaOS.network
-      });
+      this.context = {
+        os: new BalenaOS({
+          deviceType: this.context.deviceType.slug,
+          download: {
+            type: this.options.balenaOS.download.type,
+            version: this.options.balenaOS.download.version,
+            source: this.options.balenaOS.download.source
+          },
+          network: this.options.balenaOS.network
+        })
+      };
 
-      const worker = new Worker('main worker', deviceType.slug, {
-        devicePath: options.worker.device
-      });
+      this.context = {
+        worker: new Worker('main worker', this.context.deviceType.slug, {
+          devicePath: this.options.worker.device
+        })
+      };
 
-      await os.fetch(options.tmpdir);
+      await this.context.os.fetch(this.options.tmpdir);
 
-      os.balena.configJson = await sdk.getDeviceOSConfiguration(
-        uuid,
-        deviceApiKey,
-        os.image.version
+      this.context.os.balena.configJson = await this.context.balena.sdk.getDeviceOSConfiguration(
+        this.context.balena.uuid,
+        this.context.deviceApiKey,
+        this.context.os.image.version
       );
 
-      await worker.ready();
-      await worker.flash(os);
-      await worker.on();
-      teardown.register(() => {
-        return worker.off();
+      this.context = {
+        balena: { sync: require(join(this.frameworkPath, 'components', 'balena', 'sync')) }
+      };
+
+      await this.context.worker.ready();
+      await this.context.worker.flash(this.context.os);
+      await this.context.worker.on();
+      this.teardown.register(() => {
+        return this.context.worker.off();
       });
 
       console.log('Waiting for device to be online');
-      await utils.waitUntil(() => {
-        return sdk.isDeviceOnline(uuid);
+      await this.context.utils.waitUntil(() => {
+        return this.context.balena.sdk.isDeviceOnline(this.context.balena.uuid);
       });
-
-      return {
-        balena: {
-          application: options.balena.application,
-          sdk,
-          uuid,
-          sync: require(join(root, 'lib', 'components', 'balena', 'sync'))
-        },
-        utils,
-        os,
-        worker,
-        sshKeyPath,
-        deviceType,
-        teardown,
-        tmpdir: options.tmpdir
-      };
-    }).catch(async error => {
-      await teardown.run(setImmediate);
-      throw error;
-    });
-  },
-  tests: [
-    'os-file-format.js',
-    'device-reportOsVersion.js',
-    //  'hostapp-update.js',
-    'balena-sync.js',
-    'push-container.js',
-    'balena-device-progress.js',
-    'update-supervisor-through-api.js',
-    'push-multicontainer.js',
-    'move-device-between-applications.js',
-    'reload-supervisor.js',
-    'chrony-ntp-time-sync',
-    //  'kernel-module-build',
-    'i-bluetooth-test.js',
-    'i-enter-container.js',
-    'i-kernel-splash-screen.js',
-    'i-identification-led.js',
-    'i-balena-splash-screen.js',
-    'i-rpi-serial-uart0.js',
-    'i-rpi-serial-uart1.js',
-    'i-hdmi-uart5.js',
-    'i-local-mode',
-    'i-modem',
-    'i-preload',
-    'i-hostapp-update-old-to-new',
-    'i-http-proxy',
-    'i-socks-proxy',
-    'i-override-lock',
-    'i-persistent-logging',
-    'i-no-deltas',
-    'i-with-deltas',
-    'i-provision-with-deltas'
-  ]
-};
+    },
+    tests: [
+      'os-file-format.js',
+      'device-reportOsVersion.js',
+      //  'hostapp-update.js',
+      'balena-sync.js',
+      'push-container.js',
+      'balena-device-progress.js',
+      'update-supervisor-through-api.js',
+      'push-multicontainer.js',
+      'move-device-between-applications.js',
+      'reload-supervisor.js',
+      'chrony-ntp-time-sync',
+      //  'kernel-module-build',
+      'i-bluetooth-test.js',
+      'i-enter-container.js',
+      'i-kernel-splash-screen.js',
+      'i-identification-led.js',
+      'i-balena-splash-screen.js',
+      'i-rpi-serial-uart0.js',
+      'i-rpi-serial-uart1.js',
+      'i-hdmi-uart5.js',
+      'i-local-mode',
+      'i-modem',
+      'i-preload',
+      'i-hostapp-update-old-to-new',
+      'i-http-proxy',
+      'i-socks-proxy',
+      'i-override-lock',
+      'i-persistent-logging',
+      'i-no-deltas',
+      'i-with-deltas',
+      'i-provision-with-deltas'
+    ]
+  }
+];
