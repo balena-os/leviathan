6e8b48d16a408ba573f0ca5ec69972f252d4229d
rcooke-warwick <ryan@balena.io>
2021-08-17 12:03:57 +0100

    start qemu in child process
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- worker/lib/workers/qemu.ts
+++ worker/lib/workers/qemu.ts
@@ -1,6 +1,6 @@
 import * as Bluebird from 'bluebird';
 import * as retry from 'bluebird-retry';
-import { ChildProcess, spawn } from 'child_process';
+import { exec,ChildProcess, spawn } from 'child_process';
 import * as sdk from 'etcher-sdk';
 import { EventEmitter } from 'events';
 import * as libvirt from 'libvirt';
@@ -26,6 +26,8 @@ class Qemu extends EventEmitter implements Leviathan.Worker {
 	private references: { domain?: any; network?: any; pool?: any };
 	private internalState: Leviathan.WorkerState = { network: {} };
 	private screenCapturer: ScreenCapture;
+	private qemuProc?: ChildProcess;
+
 
 	constructor(options: Leviathan.Options) {
 		super();
@@ -69,337 +71,16 @@ class Qemu extends EventEmitter implements Leviathan.Worker {
 			.substring(2, 10);
 	}
 
-	private getNetworkConf({ id, nat }: { id: string; nat: boolean }): string {
-		const conf: xml.Element = {
-			elements: [
-				{
-					type: 'element',
-					name: 'network',
-					elements: [
-						{
-							type: 'element',
-							name: 'name',
-							elements: [{ type: 'text', text: id }],
-						},
-						{
-							type: 'element',
-							name: 'domain',
-							attributes: { name: id },
-						},
-
-						{
-							type: 'element',
-							name: 'ip',
-							attributes: {
-								address: '192.168.100.1',
-								netmask: '255.255.255.0',
-							},
-							elements: [
-								{
-									type: 'element',
-									name: 'dhcp',
-									elements: [
-										{
-											type: 'element',
-											name: 'range',
-											attributes: {
-												start: '192.168.100.128',
-												end: '192.168.100.254',
-											},
-										},
-									],
-								},
-							],
-						},
-					],
-				},
-			],
-		};
-
-		// Even if our conf is constant, let s satisfy the types requirments
-		if (
-			nat != null &&
-			nat &&
-			conf.elements != null &&
-			conf.elements[0].elements != null
-		) {
-			conf.elements[0].elements.push({
-				type: 'element',
-				name: 'forward',
-				attributes: { mode: 'nat' },
-			});
-		}
-
-		return xml.js2xml(conf);
-	}
-
-	private async getDomainConf({ id }: { id: string }): Promise<string> {
-		const conf: xml.Element = {
-			elements: [
-				{
-					type: 'element',
-					name: 'domain',
-					attributes: { type: 'kvm', id: '1' },
-					elements: [
-						{
-							type: 'element',
-							name: 'name',
-							elements: [{ type: 'text', text: id }],
-						},
-						{
-							type: 'element',
-							name: 'memory',
-							attributes: { unit: 'KiB' },
-							elements: [{ type: 'text', text: '1048576' }],
-						},
-						{
-							type: 'element',
-							name: 'resource',
-							elements: [
-								{
-									type: 'element',
-									name: 'partition',
-									elements: [{ type: 'text', text: '/machine' }],
-								},
-							],
-						},
-						{
-							type: 'element',
-							name: 'os',
-							elements: [
-								{
-									type: 'element',
-									name: 'type',
-									attributes: { arch: 'x86_64', machine: 'pc' },
-									elements: [{ type: 'text', text: 'hvm' }],
-								},
-								{ type: 'element', name: 'boot', attributes: { dev: 'hd' } },
-							],
-						},
-						{
-							type: 'element',
-							name: 'on_reboot',
-							elements: [{ type: 'text', text: 'restart' }],
-						},
-						{
-							type: 'element',
-							name: 'on_shutdown',
-							elements: [{ type: 'text', text: 'shutdown' }],
-						},
-						{
-							type: 'element',
-							name: 'devices',
-							elements: [
-								{
-									type: 'element',
-									name: 'emulator',
-									elements: [
-										{ type: 'text', text: '/usr/bin/qemu-system-x86_64' },
-									],
-								},
-								{
-									type: 'element',
-									name: 'disk',
-									attributes: { type: 'file', device: 'disk' },
-									elements: [
-										{
-											type: 'element',
-											name: 'driver',
-											attributes: { name: 'qemu', type: 'raw' },
-										},
-										{
-											type: 'element',
-											name: 'source',
-											attributes: {
-												file: this.image,
-											},
-										},
-										{ type: 'element', name: 'backingStore' },
-										{
-											type: 'element',
-											name: 'target',
-											attributes: { dev: 'hda', bus: 'ide' },
-										},
-										{
-											type: 'element',
-											name: 'alias',
-											attributes: { name: 'ide0-0-0' },
-										},
-										{
-											type: 'element',
-											name: 'address',
-											attributes: {
-												type: 'drive',
-												controller: '0',
-												bus: '0',
-												target: '0',
-												unit: '0',
-											},
-										},
-									],
-								},
-							],
-						},
-					],
-				},
-			],
-		};
-
-		if (this.screenCapturer != null) {
-			// The top level of the xml is one element called domain
-			if (conf.elements != null && conf.elements[0].elements != null) {
-				const devices =
-					conf.elements[0].elements[
-						conf.elements[0].elements.findIndex(element => {
-							return element.name === 'devices';
-						})
-					];
-
-				if (devices.elements != null) {
-					devices.elements.push({
-						type: 'element',
-						name: 'graphics',
-						attributes: {
-							type: 'vnc',
-							port: this.screenCapturer.source.options.port,
-							listen: this.screenCapturer.source.options.host,
-						},
-					});
-				}
-			}
-		}
-
-		if (
-			this.references.network != null &&
-			conf.elements != null &&
-			conf.elements[0].elements != null
-		) {
-			const reference = conf.elements[0].elements.find(
-				(element: xml.Element) => {
-					return element.name === 'devices';
-				},
-			);
-
-			if (reference != null && reference.elements != null) {
-				reference.elements.push({
-					type: 'element',
-					name: 'interface',
-					attributes: { type: 'network' },
-					elements: [
-						{
-							type: 'element',
-							name: 'mac',
-							attributes: { address: '52:54:00:f5:ae:e9' },
-						},
-						{
-							type: 'element',
-							name: 'source',
-							attributes: {
-								network: await this.references.network.getNameAsync(),
-								bridge: 'virbr0',
-							},
-						},
-						{
-							type: 'element',
-							name: 'target',
-							attributes: { dev: 'vnet0' },
-						},
-						{
-							type: 'element',
-							name: 'model',
-							attributes: { type: 'e1000' },
-						},
-						{
-							type: 'element',
-							name: 'alias',
-							attributes: { name: 'net0' },
-						},
-						{
-							type: 'element',
-							name: 'address',
-							attributes: {
-								type: 'pci',
-								domain: '0x0000',
-								bus: '0x00',
-								slot: '0x03',
-								function: '0x0',
-							},
-						},
-					],
-				});
-			}
-		}
-
-		return xml.js2xml(conf);
-	}
-
-	private getStoragePoolConf({ id }: { id: string }): string {
-		return xml.js2xml({
-			elements: [
-				{
-					type: 'element',
-					name: 'pool',
-					attributes: { type: 'dir' },
-					elements: [
-						{
-							type: 'element',
-							name: 'name',
-							elements: [{ type: 'text', text: id }],
-						},
-						{
-							type: 'element',
-							name: 'target',
-							elements: [
-								{
-									type: 'element',
-									name: 'path',
-									elements: [{ type: 'text', text: dirname(this.image) }],
-								},
-							],
-						},
-					],
-				},
-			],
-		});
-	}
-
+	
 	public async setup(): Promise<void> {
-		this.libvirtdProc = spawn('libvirtd', {
-			stdio: 'ignore',
-			env: assignIn(process.env, {
-				DBUS_SYSTEM_BUS_ADDRESS: 'unix:path=/host/run/dbus/system_bus_socket',
-			}),
-		});
-		this.virtlogdProc = spawn('virtlogd', {
-			stdio: 'ignore',
-			env: assignIn(process.env, {
-				DBUS_SYSTEM_BUS_ADDRESS: 'unix:path=/host/run/dbus/system_bus_socket',
-			}),
-		});
-
-		this.hypervisor = new libvirt.Hypervisor('qemu:///system');
-
-		await retry(
-			async () => {
-				await fs.stat('/var/run/libvirt/libvirt-sock');
-				await fs.stat('/var/run/libvirt/virtlogd-sock');
-				await this.hypervisor.connectAsync();
-			},
-			{ throw_original: true, interval: 1000, max_tries: 30 },
-		);
-
-		this.references.pool = await this.hypervisor.createStoragePoolAsync(
-			this.getStoragePoolConf({
-				id: Qemu.generateId(),
-			}),
-		);
-
 		manageHandlers(this.signalHandler, {
 			register: true,
 		});
 	}
 
 	public async teardown(signal?: NodeJS.Signals): Promise<void> {
+		this.qemuProc?.kill()
+		
 		if (this.screenCapturer != null) {
 			await this.screenCapturer.teardown();
 		}
@@ -450,67 +131,21 @@ class Qemu extends EventEmitter implements Leviathan.Worker {
 	}
 
 	public async flash(stream: Stream.Readable): Promise<void> {
-		this.activeFlash = new Bluebird(async (resolve, reject) => {
-			await this.powerOff();
-
-			const source = new sdk.sourceDestination.SingleUseStreamSource(stream);
-
-			const destination = new sdk.sourceDestination.File(
-				this.image,
-				sdk.sourceDestination.File.OpenFlags.ReadWrite,
-			);
-
-			await sdk.multiWrite.pipeSourceToDestinations(
-				source,
-				[destination],
-				(_destination, error) => {
-					reject(error);
-				},
-				(progress: sdk.multiWrite.MultiDestinationProgress) => {
-					this.emit('progress', progress);
-				},
-				true,
-			);
-			resolve();
-		});
-
-		await this.activeFlash;
-		this.activeFlash = undefined;
+		await Bluebird.resolve()
 	}
 
 	public async powerOn(): Promise<void> {
-		this.references.domain = await this.hypervisor.createDomainAsync(
-			await this.getDomainConf({
-				id: Qemu.generateId(),
-			}),
-		);
+		this.qemuProc = exec(`qemu-system-x86_64 -drive file=/data/os.img,media=disk,cache=none,format=raw -net nic,model=virtio -net user,hostfwd=tcp::60022-:22 -m 512 -nographic -machine type=pc,accel=kvm -smp 4 -cpu host`)
 	}
 
 	public async powerOff(): Promise<void> {
-		if (this.references.domain != null) {
-			await this.references.domain.destroyAsync();
-			this.references.domain = undefined;
-		}
+		this.qemuProc?.kill()
 	}
 
 	public async network(configuration: {
 		wired?: { nat: boolean };
 	}): Promise<void> {
-		if (configuration != null && configuration.wired != null) {
-			this.references.network = await this.hypervisor.createNetworkAsync(
-				this.getNetworkConf({
-					id: Qemu.generateId(),
-					nat: configuration.wired.nat,
-				}),
-			);
-			this.internalState.network = {
-				wired: await this.references.network.getBridgeNameAsync(),
-			};
-		} else if (this.references.network.wired != null) {
-			await this.references.network.destroyAsync();
-			this.references.network = undefined;
-			this.internalState.network.wired = undefined;
-		}
+		await Bluebird.resolve()
 	}
 
 	public async captureScreen(
