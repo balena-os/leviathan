e024650e0ec017744cd9d0d3aa6b0db9fca889d5
Joseph Kogut <joseph@balena.io>
2022-02-01 23:27:46 +0000

    core: test: proxy to context to shorten test code
    Return a proxy of this object that will forward access to properties
    present in the context object to that object.
    
    This allows us to shorten, e.g.:
    
      this.context.get().worker.executeCommandInHostOS(...)
    
    To just:
    
      this.worker.executeCommandInHostOS(...)
    
    Change-type: patch
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- core/lib/common/test.js
+++ core/lib/common/test.js
@@ -43,6 +43,26 @@ module.exports = class Test {
 
 		this.archiver = Archiver;
 		this.context = new Context(this.suite.context);
+
+		/* Return a proxy of this object that will forward access to properties
+		* present in the context object to that object.
+		*
+		* This allows us to shorten, e.g.:
+		*
+		* this.context.get().worker.executeCommandInHostOS(...)
+		*
+		* To just:
+		*
+		* this.worker.executeCommandInHostOS(...)
+		*/
+		return new Proxy(this, {
+			get: function(target, prop, receiver) {
+				const context = target.context.get();
+				return prop in context
+					? context[prop]
+					: Reflect.get(target, prop);
+			}
+		});
 	}
 
 	log(message) {
