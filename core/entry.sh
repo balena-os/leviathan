5e04208a9f07567c6aa7fdc55dc95d2cacb6c8c3
Kyle Harding <kyle@balena.io>
2022-12-01 15:20:26 -0500

    Avoid conflicting docker networks
    The `make subnet` command can be called on Jenkins and similar to generate
    a unique subnet that is not in use.
    
    The DinD daemon in the core service will also start with a non-default
    subnet.
    
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- core/entry.sh
+++ core/entry.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 rm -rf /var/run/docker 2>/dev/null || true
 rm -f /var/run/docker.sock 2>/dev/null || true
 rm -f /var/run/docker.pid 2>/dev/null || true
 
-dockerd &
+dockerd --bip 172.64.0.1/16 &
 
 eval $(ssh-agent)
 node lib/main.js
